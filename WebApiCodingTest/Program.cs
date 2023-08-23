32967f586927794be240a08d626c517aca87b269
subhani2110 <masoodsubhani21@gmail.com>
2023-08-23 19:19:35 +0500

    Initial commit

---
+++ WebApiCodingTest/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapPost("/add", (int num1, int num2) =>
+{
+    return num1 + num2;
+})
+.WithName("add");
+
+app.Run();
